7eb42d956799f2f0a7b331e31d5ae7d841bff5ac
Joshua Burton <88261197+jrock4802@users.noreply.github.com>
2021-08-01 04:19:50 +0000

    Changed API key

--- index.js
+++ index.js
@@ -6,109 +6,109 @@ const prefix = '!';
 const fs = require("fs");
 
 db.get("token").then(value => {
-client.login(value);
+  client.login(value);
 });
 
 client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./cmds/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./cmds/${file}`);
- 
-    client.commands.set(command.name,command);
+for (const file of commandFiles) {
+  const command = require(`./cmds/${file}`);
+
+  client.commands.set(command.name, command);
 }
-client.on('ready' , () => {
-    console.log("Connected as "+ client.user.tag)
-    var guildlist = client.guilds.cache.size
-    client.user.setActivity(`${guildlist} Servers! | !help`, {type: "WATCHING"})
+client.on('ready', () => {
+  console.log("Connected as " + client.user.tag)
+  var guildlist = client.guilds.cache.size
+  client.user.setActivity(`${guildlist} Servers! | !help`, { type: "WATCHING" })
 })
 db.get("key").then(value => {
-const key = value
+  const key = value
 
-client.on('message', message => {
-  let args = message.content.substring(prefix.length).split(" ");
-try {
- if(message.member.roles.cache.some(r=>["Verified"].includes(r.name))) {
-  client.commands.get('auto_update').execute(message, key, args);
-   console.log(message.author + "from " + `${message.guild.name}`)
-   }
-} catch {return}
-  switch (args[0]) {
-      
-    case "v":
-      client.commands.get('v').execute(message, key);
-      
-    break;
+  client.on('message', message => {
+    let args = message.content.substring(prefix.length).split(" ");
+    try {
+      if (message.member.roles.cache.some(r => ["Verified"].includes(r.name))) {
+        client.commands.get('auto_update').execute(message, key, args);
+        console.log(message.author + "from " + `${message.guild.name}`)
+      }
+    } catch { return }
+    switch (args[0]) {
 
-    case "g":
-      client.commands.get('guild').execute(message, key);
-    break;
-      
-    case "help":
-      client.commands.get('help').execute(message, args);
-    break;
+      case "v":
+        client.commands.get('v').execute(message, key);
 
-    case "setup":
-      client.commands.get('setup').execute(message, key);
-    break;
-      
-    case "bw":
-      client.commands.get('bedwars').execute(message, key);
-    break;
+        break;
 
-    case "bb":
-      client.commands.get('build_battle').execute(message, key);
-    break;
+      case "g":
+        client.commands.get('guild').execute(message, key);
+        break;
 
-    case "sw":
-      client.commands.get('skywars').execute(message, key);
-    break;
+      case "help":
+        client.commands.get('help').execute(message, args);
+        break;
 
-    case "mm":
-      client.commands.get('murder_mystery').execute(message, key);
-    break;
+      case "setup":
+        client.commands.get('setup').execute(message, key);
+        break;
 
-    case "sh":
-      client.commands.get('super_smash').execute(message, key);
-    break;
+      case "bw":
+        client.commands.get('bedwars').execute(message, key);
+        break;
 
-    case "pb":
-      client.commands.get('paintball').execute(message, key);
-    break;
+      case "bb":
+        client.commands.get('build_battle').execute(message, key);
+        break;
 
-    case "pit":
-      client.commands.get('pit').execute(message, key);
-    break;
+      case "sw":
+        client.commands.get('skywars').execute(message, key);
+        break;
 
-    case "cvc":
-      client.commands.get('cops_vs_criminals').execute(message, key);
-    break;
+      case "mm":
+        client.commands.get('murder_mystery').execute(message, key);
+        break;
 
-    case "duels":
-      client.commands.get('duels').execute(message, key);
-    break;
+      case "sh":
+        client.commands.get('super_smash').execute(message, key);
+        break;
 
-    case "quake":
-      client.commands.get('quake').execute(message, key);
-    break;
+      case "pb":
+        client.commands.get('paintball').execute(message, key);
+        break;
 
-    case "profile":
-      client.commands.get('profile').execute(message, key);
-    break;
+      case "pit":
+        client.commands.get('pit').execute(message, key);
+        break;
 
-    case "walls":
-      client.commands.get('walls').execute(message, key);
-    break;
+      case "cvc":
+        client.commands.get('cops_vs_criminals').execute(message, key);
+        break;
 
-    case "mw":
-      client.commands.get('mega_walls').execute(message, key);
-    break;
+      case "duels":
+        client.commands.get('duels').execute(message, key);
+        break;
 
-    case "ping":
-      client.commands.get('ping').execute(message, client, Discord);
-    break;
+      case "quake":
+        client.commands.get('quake').execute(message, key);
+        break;
+
+      case "profile":
+        client.commands.get('profile').execute(message, key);
+        break;
+
+      case "walls":
+        client.commands.get('walls').execute(message, key);
+        break;
+
+      case "mw":
+        client.commands.get('mega_walls').execute(message, key);
+        break;
+
+      case "ping":
+        client.commands.get('ping').execute(message, client, Discord);
+        break;
 
 
-  }
-});
+    }
+  });
 });
